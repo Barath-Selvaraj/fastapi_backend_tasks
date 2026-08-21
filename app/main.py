fd994f4fd5d494b83c4dd955bd31f322d7d7b84e
Barath Selvaraj <vinaybarathselvaraj@gmail.com>
2026-08-21 04:53:14 +0000

    Add Docker setup and database initialization

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 
 from app.api.routes.products import router as products_router
 from app.api.routes.categories import router as categories_router
-
+from app.db.init_db import init_db
 
 app = FastAPI()
 
+init_db()
 
 app.include_router(products_router)
 app.include_router(categories_router)
